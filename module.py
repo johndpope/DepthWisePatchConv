906d4a755789e71fa08f7ec055add555acae197a
John D. Pope <jp@bellgeorge.com>
2024-05-20 21:46:55 +1000

    Create module.py

---
+++ module.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class DepthWisePatchConv(nn.Module):
+    def __init__(self, splits: int = 4, conv2d: nn.Conv2d = None, *args, **kwargs):
+        super(PatchPointwiseConv2d, self).__init__()
+        if conv2d is not None:
+            self.conv2d = conv2d
+            self.splits = splits
+        else:
+            self.conv2d = nn.Conv2d(*args, **kwargs, groups=kwargs['in_channels'])
+            self.pointwise = nn.Conv2d(kwargs['in_channels'], kwargs['out_channels'], 1)
+            self.splits = splits
+
+    def forward(self, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+        b, c, h, w = x.shape
+        if c * h * w >= (1 << 30):
+            assert h % self.splits == 0
+            split_size = h // self.splits
+            x_permuted = x.view(b, c, self.splits, split_size, w).permute(0, 2, 1, 3, 4)
+            padding_bak = self.conv2d.padding
+            self.conv2d.padding = (0, self.conv2d.padding[1])
+            output = torch.zeros(b, self.splits, self.conv2d.out_channels, split_size + 2 * self.conv2d.padding[0], w, device=x.device)
+            for i in range(self.splits):
+                if i == 0:
+                    x_padded = F.pad(
+                        x_permuted[:, i],
+                        (0, 0, self.conv2d.padding[0], self.conv2d.padding[0]),
+                        mode="constant" if self.conv2d.padding_mode == "zeros" else self.conv2d.padding_mode,
+                        value=0,
+                    )
+                else:
+                    x_padded[:, :, : self.conv2d.padding[0]] = output[:, i - 1, :, -2 * self.conv2d.padding[0] : -self.conv2d.padding[0]]
+                    x_padded[:, :, -self.conv2d.padding[0] :] = x_permuted[:, i, :, : self.conv2d.padding[0]]
+                output[:, i] = self.pointwise(self.conv2d(x_padded, *args, **kwargs))
+            self.conv2d.padding = padding_bak
+            output = output.permute(0, 2, 1, 3, 4).reshape(b, self.conv2d.out_channels, -1, w)
+            return output
+        else:
+            return self.pointwise(self.conv2d(x, *args, **kwargs))
